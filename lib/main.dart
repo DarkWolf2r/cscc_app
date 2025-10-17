066d8999176e6480497663f9b1c65e176ce85042
DarkWolf <laouar.romaissa.info@gmail.com>
2025-10-17 20:28:36 +0100

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  // final FirebaseFirestore firestore = FirebaseFirestore.instance;
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
