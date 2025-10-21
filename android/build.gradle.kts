9fbd884d52cb1b108f7a26467899558331fce9f6
tarekch12 <cht1485@gmail.com>
2025-10-21 19:00:44 +0100

    delete web floder

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,7 @@ val newBuildDir: Directory =
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
+
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
