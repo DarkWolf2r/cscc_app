b2ebca3fd973796bc6f0c1492cb634da3f75c257
DarkWolf <laouar.romaissa.info@gmail.com>
2025-10-22 21:26:53 +0100

    hh

--- lib/global_variable.dart
+++ lib/global_variable.dart
@@ -5,7 +5,6 @@
 // import 'package:cscc_app/screens/notification_screen.dart';
 // import 'package:cscc_app/screens/profile_screen.dart';
 
-// // هذا هو المتغير اللي يستعمله HomePage
 // const List<Widget> homeScreenItems = [
 //   HomeScreen(),
 //   SearchScreen(),
